753556beecad55a8b5d8ce972f30f79e20d50adf
Vinicius Azambuja <vnazambuja@gmail.com>
2026-01-09 16:18:49 -0300

    finished lec16, recursion

--- lectures/lec16.py
+++ lectures/lec16.py
@@ -1,6 +1,9 @@
 ###############
 ## Fibonacci with a dictionary
 #################
+from prompt_toolkit.filters import emacs_selection_mode
+
+
 def fib_recur(n):
     if n == 1 or n == 2:
         return 1
@@ -41,9 +44,9 @@ def score_count(x):
         return score_count(x - 1) + score_count(x - 2) + score_count(x - 3)
 
 
-# print(score_count(4))  # prints 6
-# print(score_count(6))  # prints 20
-# print(score_count(13))  # prints 1431
+print(score_count(4))  # prints 6
+print(score_count(6))  # prints 20
+print(score_count(13))  # prints 1431
 
 
 ## sum of a list, iterative
@@ -73,17 +76,18 @@ test = [30, 40, 50]
 ############### YOU TRY IT ###############
 # Modify the code we wrote to return the total length
 # of all strings inside L:
+# VAO: my code
 
 
 def total_len_recur(L):
     if len(L) == 1:
-        return ____
+        return len(L[0])  # VAO: I had return 1, caught it in time
     else:
-        return ____
+        return len(L[0]) + total_len_recur(L[1:])
 
 
 test = ["ab", "c", "defgh"]
-# print(total_len_recur(test))  # should print 8
+print(total_len_recur(test))  # should print 8
 
 ##########################################
 
@@ -113,10 +117,15 @@ def in_list(L, e):
     if len(L) == 1:
         return L[0] == e
     else:
-        if L[0] == e:
+        if L[0] == e:  # VAO: why do you need the base case above?
+            # it just checks if it is a one element list
             return True
         else:
             return in_list(L[1:], e)
+
+
+test = [2, 1, 5, 8]
+print(in_list(test, 1))  # bad!
 
 
 ## another correct (look at the last elem in the list)
@@ -160,14 +169,14 @@ def flatten(L):
     if len(L) == 1:
         return L[0]
     else:
-        return L[0] + flatten(L[1:])
+        return L[0] + flatten(L[1:])  # concatenation, adds elements into same list
 
 
 test = [[1]]
 # print(flatten(test))
 
 test = [[1, 2], [3, 4], [9, 8, 7]]
-# print(flatten(test))
+print(flatten(test))
 
 
 ################### YOU TRY IT ##############
@@ -179,13 +188,20 @@ def in_lists_of_list(L, e):
     Hint, the in operator is useful here, i.e. e in something
     """
     # your code here
+    # VAO: my code
+    if len(L) == 1:
+        return e in L[0]  # L[0], not L, because it's still a list of lists
+    elif e in L[-1]:
+        return True
+    else:
+        return in_lists_of_list(L[:-1], e)
 
 
 test = [[1, 2], [3, 4], [5, 6, 7]]
-# print(in_lists_of_list(test, 3))  # prints True
+print(in_lists_of_list(test, 3))  # prints True
 
 test = [[1, 2], [3, 4], [5, 6, 7]]
-# print(in_lists_of_list(test, 0))  # prints False
+print(in_lists_of_list(test, 0))  # prints False
 
 ###############################################
 
@@ -304,13 +320,19 @@ def in_lists_of_list(L, e):
 ######################################################
 # Q1. Memoize the code to find possible scores in basketball
 def score_count(x, d):
-    pass
+    if x in d:  # VAO: all the base cases are moved to the dict
+        return d[x]
+    else:
+        # VAO: do the calculations w/o returning anything
+        answer = score_count(x - 1, d) + score_count(x - 2, d) + score_count(x - 3, d)
+        d[x] = answer
+        return answer
 
 
 d = {1: 1, 2: 2, 3: 3}
-# print(score_count(4, d))  # prints 6
-# print(score_count(6, d))  # prints 20
-# print(score_count(13, d))  # prints 1431
+print(score_count(4, d))  # prints 6
+print(score_count(6, d))  # prints 20
+print(score_count(13, d))  # prints 1431
 
 
 # Q2.
@@ -323,14 +345,29 @@ def in_list_of_lists_mod(L, e):
     sublists of L and False otherwise.
     """
     # your code here
+    # VAO: my code
+    if len(L) == 1 and type(L[0]) is not list:
+        return e == L[0]
+    elif len(L) == 1 and type(L[10]) is list:
+        return e in L
+    elif type(L[0]) is list:
+        if e in L[0]:
+            return True
+        else:
+            return in_list_of_lists_mod(L[1:], e)
+    elif type(L[0]) is not list:
+        if e == L[0]:
+            return True
+        else:
+            return in_list_of_lists_mod(L[1:], e)
 
 
-# test = [[1,2],3,4,5,6,7]
-# print(in_list_of_lists_mod(test, 3))  # prints True
-# test = [[1,2],[3,4,5],6,7]
-# print(in_list_of_lists_mod(test, 3))  # prints True
-# test = [[1,2],[3,4,5],6,7]
-# print(in_list_of_lists_mod(test, 10))  # prints False
+test = [[1, 2], 3, 4, 5, 6, 7]
+print(in_list_of_lists_mod(test, 3))  # prints True
+test = [[1, 2], [3, 4, 5], 6, 7]
+print(in_list_of_lists_mod(test, 3))  # prints True
+test = [[1, 2], [3, 4, 5], 6, 7]
+print(in_list_of_lists_mod(test, 10))  # prints False
 
 
 # Q3.
@@ -341,12 +378,20 @@ def my_deepcopy(L):
     contains copies (recursively) of every sublist
     """
     # your code here
+    # VAO: my code; totally wrong, couldn't make it work
+    if type(L[0]) is not list:
+        return [L[0]] + my_deepcopy(L[1:])  # L[0] kept as list element
+    else:
+        return [my_deepcopy(L[0])] + my_deepcopy(
+            L[1:]
+        )  # since L[0] is a list, you run it through again to dive into deeper levels,
+    # returning it as list elements back again (hence the [])
 
 
-# myL = ["abc", ['d'], ['e', ['f', 'g']]]
-# my_newL = my_deepcopy(myL)
-# print(myL)
-# print(my_newL)
+myL = ["abc", ["d"], ["e", ["f", "g"]]]
+my_newL = my_deepcopy(myL)
+print(myL)
+print(my_newL)
 # myL[2][1][0] = 1
 # print(myL)      # should be ['abc', ['d'], ['e', [1, 'g']]]
 # print(my_newL)  # should be ['abc', ['d'], ['e', ['f', 'g']]]
@@ -359,17 +404,23 @@ def f(L):
     Returns the letter earliest in the alphabet."""
     if len(L) == 1:
         return L[0]
+    # else:
+    #     if L[0] < f((L[0])):
+    #         return L[0]
+    # VAO: my code, I changed it quite a bit, but works fine
+    elif L[0] < L[-1]:
+        return f(L[:-1])
     else:
-        if L[0] < f((L[0])):
-            return L[0]
+        return f(L[1:])
 
 
-# print(f(['z', 'a', 'b', 'c', 'd']))  # should print 'a'
+print(f(["z", "a", "b", "c", "d"]))  # should print 'a'
 
 
 def g(L, e):
     """L is list of ints, e is an int
     Returns a count of how many times e occurrs in L"""
+    # VAO: these are the base cases, no change
     if len(L) == 0:
         return 0
     elif len(L) == 1:
@@ -377,15 +428,16 @@ def g(L, e):
             return 1
         else:
             return 0
+    # VAO: now for the recursive part
     else:
         if L[0] == e:
-            1 + g(L[1:], e)
+            return 1 + g(L[1:], e)  # VAO: missing a return
         else:
-            return 1
+            return g(L[1:], e)
 
 
-# print(g([1,2,3,1], 1))     # should print 2
-# print(g([1,1,2,3,1,1], 1)) # should print 4
+print(g([1, 2, 3, 1], 1))  # should print 2
+print(g([1, 1, 2, 3, 1, 1], 1))  # should print 4
 
 
 def h(L, e):
@@ -396,17 +448,24 @@ def h(L, e):
     if len(L) == 0:
         return 0
     else:
-        if type(L[0]) == int:
+        if type(L[0]) is int:
             if L[0] == e:
                 return 1 + h(L[1:], e)
             else:
                 return h(L[1:], e)
-        elif type(L[0]) == list:
-            return h(L[1:], e)
+        elif type(L[0]) is list:
+            if e in L[0]:
+                return h(L[0], e) + h(
+                    L[1:], e
+                )  # you need the first element to call the list again
+            # it might be a list of lists, with additional target elements
+            # same as my_deepcopy(), deep_rev() and others
+            else:
+                return h(L[1:], e)
 
 
-# print(h([1,2,[3],1], 1))        # should print 2
-# print(h([1,2,[3,1,[1,[1]]]], 1))  # should print 4
+print(h([1, 2, [3], 1], 1))  # should print 2
+print(h([1, 2, [3, 1, [1, [1]]]], 1))  # should print 4
 
 
 # finger exercise: Implement the functions that meets the specifications below
@@ -416,7 +475,19 @@ def flatten(L):
     Returns a copy of L, which is a flattened version of L
     """
     # Your code here
+    # VAO: my code; can't figure out how to take the [3] out of nested list
+    # nor can I understand why it's there
+    if len(L) == 1:
+        return L
+    else:
+        if type(L[0]) is not list:
+            return [L[0]] + flatten(L[1:])  # I guess the issue is here?
+        elif type(L[0]) is list:
+            return flatten(L[0]) + flatten(L[1:])
 
+
+# VAO: the solution uses .append() and .extend(), which is fine, but now how
+# the classes have been tought, were we were expected to not use them
 
 # Examples:
 L = [[1, 4, [6], 2], [[[3]], 2], 4, 5]
